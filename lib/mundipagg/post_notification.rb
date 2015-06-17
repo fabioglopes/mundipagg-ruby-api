d0f82e48e5257c7c149e1ee97fe405705d02bb97
fabio lopes <fabioglopes@gmail.com>
2015-06-17 19:22:52 -0300

    fix post_notification CGI unescape

--- lib/mundipagg/post_notification.rb
+++ lib/mundipagg/post_notification.rb
@@ -10,22 +10,13 @@ module Mundipagg
 	    # @return [Hash<Symbol, String>] A hash collection containing the XML data parsed.
 		def self.ParseNotification(xml)
 
-			nori = Nori.new(:convert_tags_to => lambda { |tag| PostNotification.to_underscore(tag).to_sym })
-			xml_hash  = nori.parse(CGI::unescapeHTML(xml))
+			nori = Nori.new(:convert_tags_to => lambda { |tag| tag.snakecase.to_sym })
+			first_pass = CGI::unescape(xml)
+			xml_hash  = nori.parse(CGI::unescapeHTML(first_pass))
 
 			return xml_hash
 		end
 
-		# Converts a string in Camel Case format to lower case with underscore.
-		#
-		# @param Example: 'StatusNotification' outputs 'status_notification'
-		# @returns [String] lower case string separated with underscore
-		def self.to_underscore(camel_case_string)
-			return camel_case_string.gsub(/::/, '/').
-				gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-				gsub(/([a-z\d])([A-Z])/,'\1_\2').
-				tr("-", "_").
-				downcase
-		end
+
 	end
 end
